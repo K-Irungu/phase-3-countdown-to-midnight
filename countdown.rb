a6345e05b2d31ff5dbfdc65e37e0caae6c601124
Kevin Irungu <kevin.irungu@student.moringaschool.com>
2022-10-19 13:10:49 +0300

    Completed Assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,26 @@
 #write your code here
 
-def countdown
+def countdown (integer)
+
+   i = integer
+
+   while integer.class == Integer && i >= 1
+        puts "#{i} SECOND(S)!"
+        i = i - 1
+   end
+
+   "HAPPY NEW YEAR!"
+
+end
+
+
+def countdown_with_sleep (integer)
+    i = integer
+
+   while integer.class == Integer && i >= 1
+        puts "#{i} SECOND(S) !"
+        i = i - 1
+        sleep 1
+   end
+    
 end
